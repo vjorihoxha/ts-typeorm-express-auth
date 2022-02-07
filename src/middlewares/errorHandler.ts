f174798e2f3b72ca2bd0795a0c4309c986c2dd61
vjori <vhoxha@airfind.com>
2022-02-06 23:46:24 -0500

    feat/DEV-1 minor changes to errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-import { CustomError } from '../utils/response/custom-error/CustomError';
+import { CustomError } from 'utils/response/custom-error/CustomError';
 
 export const errorHandler = (
 	err: CustomError,
 	req: Request,
 	res: Response,
+	next: NextFunction,
 ) => {
 	return res.status(err.HttpStatusCode).json(err.JSON);
 };
